20f2e2f9452fa7b81af64ee95a48739bff3ff076
NickBwalley <nickbwalley@gmail.com>
2022-05-11 23:13:19 +0300

    creating a shared image Avatar

--- smart-social-locator/src/user/components/UsersList.js
+++ smart-social-locator/src/user/components/UsersList.js
@@ -7,14 +7,14 @@ const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found!</h2>
+        <h2>No users found.</h2>
       </div>
     );
   }
 
   return (
     <ul className="users-list">
-      {props.item.map((user) => (
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
--- smart-social-locator/src/user/pages/Users.js
+++ smart-social-locator/src/user/pages/Users.js
@@ -8,10 +8,11 @@ const Users = () => {
       id: "u1",
       name: "Nick Bwalley",
       image:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fprogramming-skills-concept-banner-workflow-group-programmers-coding-process-d-vector-isometric-illustration-image156829857&psig=AOvVaw0ojW19tdV3Caoi8_c43M_P&ust=1652308044846000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNiS26r91fcCFQAAAAAdAAAAABAD",
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
     },
   ];
+
   return <UsersList items={USERS} />;
 };
 
