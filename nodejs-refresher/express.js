930f04e5ed079ca40d202d57b2a27891235dcf67
unknown <nickbwalley@gmail.com>
2022-08-23 00:00:40 +0300

    express refresher

--- nodejs-refresher/express.js
+++ nodejs-refresher/express.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/user", (req, res, next) => {
   res.send("<h1> User:" + req.body.username + "</h1>");
 });
+
 app.get("/", (req, res, next) => {
   res.send(
     '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
