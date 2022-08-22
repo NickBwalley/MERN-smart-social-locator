92e893ed0212af5267652c9ec4c2d50d9b9c0b09
unknown <nickbwalley@gmail.com>
2022-08-22 12:02:23 +0300

    adding a login form handler

--- smart-social-locator/src/places/components/PlaceItem.js
+++ smart-social-locator/src/places/components/PlaceItem.js
@@ -8,11 +8,11 @@ import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
-  
+
   const [showConfirmModal, setShowConfirmModal] = useState(false);
-  
+
   const openMapHandler = () => setShowMap(true);
-  
+
   const closeMapHandler = () => setShowMap(false);
 
   const showDeleteWarningHandler = () => {
@@ -25,7 +25,7 @@ const PlaceItem = (props) => {
 
   const confirmDeleteHandler = () => {
     setShowConfirmModal(false);
-    console.log('DELETING...');
+    console.log("DELETING...");
   };
 
   return (
@@ -41,18 +41,27 @@ const PlaceItem = (props) => {
         <div className="map-container">
           <Map center={props.coordinates} zoom={16} />
         </div>
-      </Modal>  
+      </Modal>
       <Modal
-      show={showConfirmModal}
-      onCancel={cancelDeleteHandler}
-      header="Are you sure?" 
-      footerClass="place-item__modal-actions" footer={
-        <React.Fragment>
-          <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
-          <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
-        </React.Fragment>
-      }>
-      <p>Do you want to proceed and delete this place? Please note that this action cannot be reversed. </p>
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that this
+          action cannot be reversed.{" "}
+        </p>
       </Modal>
       <li className="place-item">
         <Card className="place-item__content">
@@ -69,7 +78,9 @@ const PlaceItem = (props) => {
               VIEW ON MAP
             </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
