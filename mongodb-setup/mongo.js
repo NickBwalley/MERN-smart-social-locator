03ddd01e6576f76aaf220cf8ea0cd8b5a40fdaec
NickBwalley <NickBwalley@gmail.com>
2022-08-28 22:53:04 +0300

    logic for creating our products

---
+++ mongodb-setup/mongo.js
@@ -0,0 +1,11 @@
+const mongoClient = require("mongodb").mongoClient;
+
+const url =
+  "mongodb+srv://nickbiiy:<wearelegion@079>@cluster0.ky96b9k.mongodb.net/products?retryWrites=true&w=majority";
+
+const createProduct = async (req, res, next) => {};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
