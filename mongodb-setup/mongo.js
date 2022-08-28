2f4dd522338ff3cb76c5aadfc5048dfd06cca5ea
NickBwalley <NickBwalley@gmail.com>
2022-08-28 23:09:14 +0300

    successfully connected to the mongoatlas db

--- mongodb-setup/mongo.js
+++ mongodb-setup/mongo.js
@@ -1,9 +1,26 @@
-const mongoClient = require("mongodb").mongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  "mongodb+srv://nickbiiy:<wearelegion@079>@cluster0.ky96b9k.mongodb.net/products?retryWrites=true&w=majority";
+  "mongodb+srv://nickbiiy:WeAreLegion079@cluster0.ky96b9k.mongodb.net/products_test?retryWrites=true&w=majority";
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data." });
+  }
+  client.close();
+
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
