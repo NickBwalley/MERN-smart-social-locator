73c76e7a54b21ba2dbb9fee34263c52d99b2b184
NickBwalley <NickBwalley@gmail.com>
2022-08-29 16:57:54 +0300

    connecting to mongoose

--- mongodb-setup/mongoose.js
+++ mongodb-setup/mongoose.js
@@ -2,11 +2,25 @@ const mongoose = require("mongoose");
 
 const Product = require("./models/product");
 
+mongoose
+  .connect(
+    "mongodb+srv://nickbiiy:WeAreLegion079@cluster0.ky96b9k.mongodb.net/products_test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 const createProduct = async (req, res, next) => {
   const createdProduct = new Product({
     name: req.body.name,
     price: req.body.price,
   });
+  const result = await createdProduct.save();
+
+  res.json(result);
 };
 
-module.createProduct = createProduct;
+exports.createProduct = createProduct;
